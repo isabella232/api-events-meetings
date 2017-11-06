d7cd0e7033fb1eed33a02e7ca29734ba14dd3703
Glenn Inn <glenninn@yahoo.com>
2017-11-05 19:47:18 -0800

    Sample Summary of JSON Messages Types
    Captured the A, D, F, and M JSON event record objects with field data.

---
+++ messageTypes.js
@@ -0,0 +1,89 @@
+a:{
+	"A1": "1",
+	"A2": "0",
+	"A3": "0",
+	"A4": "Opus 16Khz",
+	"C1": "5",
+	"C5": [{
+		"callGuid": "40f87677-a0c7-40ea-b747-afbb0e135beb",
+		"capabilities": ["AUDIO", "VIDEO", "CONTENT"],
+		"connectionGuid": "connguid:1667e631-045c-46e3-a8ad-73e3dddbb580",
+		"endpoint": "Acid"
+	}],
+	"E1": "seamguid:5950abfb-9e99-4d76-83ae-b23d419ada9e",
+	"L1": "1",
+	"L2": "4",
+	"R1": "",
+	"R2": "1",
+	"R3": "1",
+	"S1": "1",
+	"S2": "2",
+	"V1": "0",
+	"V2": "0",
+	"V3": "0",
+	"V4": "VP8",
+	"c": "40f87677-a0c7-40ea-b747-afbb0e135beb",
+	"ch": "9f4ab3ea3f05390607578ae936341446",
+	"e": "Acid",
+	"lc": true,
+	"m": "795189630:290903-210e30f8-9372-4978-a570-be7f05c80ea5,0",
+	"n": "Glenn Inn",
+	"sm": false,
+	"v": "1"
+};
+
+d:{
+	"E1": "seamguid:5950abfb-9e99-4d76-83ae-b23d419ada9e",
+	"c": "40f87677-a0c7-40ea-b747-afbb0e135beb",
+	"n": "Glenn Inn",
+	"v": "1"
+};
+
+f:{
+	"A1": "1",
+	"A2": "0",
+	"A3": "0",
+	"A4": "Opus 16Khz",
+	"A5": "Opus 16Khz",
+	"C1": "5",
+	"C5": [{
+		"callGuid": "1b6297ec-dd29-4da7-918d-fb50fc642c32",
+		"capabilities": ["AUDIO", "VIDEO", "CONTENT"],
+		"connectionGuid": "connguid:93409bb6-407a-4d00-97fa-1423d3137093",
+		"endpoint": "Carmel"
+	}],
+	"E1": "seamguid:08717d62-6331-4987-bf64-e8afab33f19c",
+	"L1": "1",
+	"L2": "4",
+	"R1": "1.0",
+	"R2": "1",
+	"R3": "1",
+	"S1": "1",
+	"S2": "2",
+	"T": "1",
+	"V1": "1",
+	"V2": "0",
+	"V3": "0",
+	"V4": "H.264 HP",
+	"V5": "720",
+	"V6": "1280",
+	"V7": "H.264 HP",
+	"V8": "720",
+	"V9": "1280",
+	"c": "1b6297ec-dd29-4da7-918d-fb50fc642c32",
+	"ch": "3988763e268c86c28751148c6744a9c0",
+	"e": "Carmel",
+	"lc": true,
+	"m": "795189630:290903-210e30f8-9372-4978-a570-be7f05c80ea5,0",
+	"n": "Glenn Inn",
+	"sm": false,
+	"v": "1"
+};
+
+m:{
+	"A2": "0",
+	"c": "9c674356-44a4-4d78-baf0-43bbdb6538d1",
+	"T": "1",
+	"E1": "seamguid:4ded9498-7ccb-447e-8aa4-4836dfd797cc"
+};
+
